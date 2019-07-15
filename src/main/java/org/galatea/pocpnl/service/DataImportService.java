c2edad5b075a307153134f19d1f9d45b91d3d270
Javier Arguello <arguello@galatea-associates.com>
2019-07-15 12:24:53 -0400

    Loading Trades from json instead of sql

--- src/main/java/org/galatea/pocpnl/service/DataImportService.java
+++ src/main/java/org/galatea/pocpnl/service/DataImportService.java
@@ -7,16 +7,19 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.galatea.pocpnl.domain.Position;
+import org.galatea.pocpnl.domain.Trade;
 import org.galatea.pocpnl.domain.Valuation;
 import org.galatea.pocpnl.repository.PositionRepository;
+import org.galatea.pocpnl.repository.TradeRepository;
 import org.galatea.pocpnl.repository.ValuationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
-
+@Slf4j
 @Service
 public class DataImportService {
 
@@ -26,15 +29,20 @@ public class DataImportService {
   @Autowired
   private ValuationRepository valuationRepository;
 
+  @Autowired
+  private TradeRepository tradeRepository;
+
   private String positionsFile = "data/positions.txt";
   private String valuationsFile = "data/valuations.txt";
+  private String tradesFile = "data/trades.txt";
 
   private Gson g = new Gson();
 
   public void importData() {
-
+    log.info("Importing data");
     importPositions(getLines(positionsFile));
     importValuations(getLines(valuationsFile));
+    importTrades(getLines(tradesFile));
 
   }
 
@@ -68,6 +76,13 @@ public class DataImportService {
     for (String json : lines) {
       Valuation valuation = g.fromJson(json, Valuation.class);
       valuationRepository.save(valuation);
+    }
+  }
+
+  private void importTrades(List<String> lines) {
+    for (String json : lines) {
+      Trade valuation = g.fromJson(json, Trade.class);
+      tradeRepository.save(valuation);
     }
   }
 
