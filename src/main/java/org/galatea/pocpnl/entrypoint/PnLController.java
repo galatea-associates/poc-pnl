f06ba09c7eeae1ca108163551a560b3a0f06b420
Javier Arguello <jas91@bu.edu>
2019-07-17 06:52:11 -0400

    Valuation Over time

--- src/main/java/org/galatea/pocpnl/entrypoint/PnLController.java
+++ src/main/java/org/galatea/pocpnl/entrypoint/PnLController.java
@@ -1,9 +1,14 @@
 package org.galatea.pocpnl.entrypoint;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.galatea.pocpnl.domain.PnL;
 import org.galatea.pocpnl.domain.Position;
+import org.galatea.pocpnl.domain.Valuation;
 import org.galatea.pocpnl.repository.PnLRepository;
 import org.galatea.pocpnl.repository.PositionRepository;
+import org.galatea.pocpnl.repository.ValuationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,26 +17,37 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PnLController {
 
-  @Autowired
-  PositionRepository positionRepository;
-  @Autowired
-  PnLRepository pnlRepository;
+	@Autowired
+	PositionRepository positionRepository;
+	@Autowired
+	PnLRepository pnlRepository;
+	@Autowired
+	ValuationRepository valuationRepository;
 
-  @GetMapping("/positions")
-  public String getAllPositions(Model model) {
-    Iterable<Position> positions = positionRepository.findAll();
+	@GetMapping("/positions")
+	public String getAllPositions(Model model) {
+		Iterable<Position> positions = positionRepository.findAll();
 
-    model.addAttribute("positions", positions);
-    return "positions2"; // view
-  }
+		model.addAttribute("positions", positions);
+		return "positions2"; // view
+	}
 
-  @GetMapping("/pnl")
-  public String getAllPnL(Model model) {
-    Iterable<PnL> results = pnlRepository.findAll();
+	@GetMapping("/valuationOverTime")
+	public String getValuationOverTime(Model model) {
+		model.addAttribute("mydata", Arrays.asList(12, 19, 3, 5, 2, 3));
 
-    model.addAttribute("results", results);
-    return "pnlResults"; // view
-  }
+		List<Valuation> valuations = valuationRepository.findAll();
+		model.addAttribute("valuations", valuations);
 
+		return "valuationOverTime"; // view
+	}
+
+	@GetMapping("/pnl")
+	public String getAllPnL(Model model) {
+		Iterable<PnL> results = pnlRepository.findAll();
+
+		model.addAttribute("results", results);
+		return "pnlResults"; // view
+	}
 
 }
